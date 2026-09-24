c81e0ea93ae03c3a8c35e8cadd0aca44da8b6069
legalwithdev <devkumarlaw@zohomail.in>
2026-09-24 19:50:48 +0530

    Telegram/Messenger fix: full patch + workflow (v2 - pichla commit truncated tha)

--- fix-tg.py
+++ fix-tg.py
@@ -10,4 +10,25 @@ def rep(path, old, new, what):
     open(path, "w", encoding="utf-8").write(src.replace(old, new))
     print("OK:", what)
 
-CLAIM_OLD
+CLAIM_OLD = '"Answers in your native language + Hindi + English.\\n\\n" +'
+CLAIM_NEW = '"I answer in whatever language you ask your question in.\\n\\n" +'
+rep("functions/webhook/telegram.js", CLAIM_OLD, CLAIM_NEW, "telegram /start 3-language claim")
+rep("functions/webhook/messenger.js", CLAIM_OLD, CLAIM_NEW, "messenger greeting 3-language claim")
+
+FB_OLDH	ÉÉÈ™]\›ˆ
+ˆ’IÛH›İİ\™HX›İ]]Û™HY]H^HZ[Z[ˆÛ›İÛYÙHÛİ™\œÈ\ÙH\™X\Î——ˆˆ
+Âˆ	İ]\ßW—˜
+Âˆ•È[˜X›Hœ™YKY›Ü›H[œİÙ\œÈÛˆS–HYØ[]Y\İ[Û‹ÛÛ›™Xİ[ˆRH[Ù[ˆ
+ÂˆŠÙYHH‘PQQKÙXİ[Ûˆ	ĞÛÛ›™Xİ[™È[ˆRHœ˜Z[‰ÊK——ˆˆ
+ÂˆTĞÓRSQT‚ˆ
+NÉÉÉÂ‘—Ó‘UÈH	ÉÉÈ™]\›ˆ
+ˆ’IÛHÛÜœKHÛİ[‰İ[œİÙ\ˆ]Û™H\İ›İÈHX\ÙHHYØZ[ˆ[ˆH[ÛY[——ˆˆ
+Âˆ“^HZ[Z[ˆ]ZXÚÈ[œİÙ\œÈÛİ™\ˆ\ÙHÛÛ[[ÛˆÜXÜÎ——ˆˆ
+Âˆ	İ]\ßW—˜
+Âˆ–[İHØ[ˆ[ÛÈH™\˜\Ú[™ÈÚ]Ù^]ÛÜ™ÈZÙH	Ù\ÜÚ]	Ë	ØÚ\]YIË	Ñ’T‰Ë	Ô•IË	ÜØØ[IÈÜˆ	ÜØ[\IË——ˆˆ
+ÂˆTĞÓRSQT‚ˆ
+NÉÉÉÂœ™\
+™[˜İ[ÛœË×ÛX‹Øœ˜Z[‹šœÈ‹—ÓÓ—Ó‘UË˜œ˜Z[‹šœÈ˜[˜XÚÈY\ÜØYÙH
+]ˆ^İ]
+HŠB‚œš[
+SÈ’STÈUÒQŠB
